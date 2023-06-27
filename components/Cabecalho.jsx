fa66eb514d3d5439b4b4faaab70a9329f5cd86ad
Victor Gabriel <vg.vg1@hotmail.com>
2023-06-27 19:00:17 -0300

    icone github cabecalho

--- components/Cabecalho.jsx
+++ components/Cabecalho.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { BsGithub } from 'react-icons/Bs';
 
 const Cabecalho = (props) => {
   return (
@@ -12,6 +13,9 @@ const Cabecalho = (props) => {
             <Nav.Link href="/deputados/" className={props.deputados}>Deputados</Nav.Link>
             <Nav.Link href="/partidos/" className={props.partidos}>Partidos</Nav.Link>
             <Nav.Link href="/proposicoes/" className={props.proposicoes}>Proposições</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="https://github.com/TotalmenteNoob/trabalhoPi" target="_black"><BsGithub size={30}/></Nav.Link>
           </Nav>
         </Container>
       </Navbar>
